0fa1f1e92e7eb05ddfaa3492a6d6356901fcf0d9
ibuku <babatundeibukun16@gmail.com>
2019-07-01 14:11:07 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+
+function zero(){
+ document.getElementById("ans").value += '0';
+
+}
+function one(){
+    document.getElementById("ans").value += '1';
+   
+   }
+   
+
+   function two(){
+    document.getElementById("ans").value += '2';
+   
+   }
+
+   function three(){
+    document.getElementById("ans").value += '3';
+   
+   }
+   function four(){
+    document.getElementById("ans").value += '4';
+   
+   }
+   function five(){
+    document.getElementById("ans").value += '5';
+   }
+   function six(){
+    document.getElementById("ans").value += '6';
+   }
+   function seven(){
+    document.getElementById("ans").value += '7';
+   }
+   function eight(){
+    document.getElementById("ans").value += '8';
+   }
+   function nine(){
+    document.getElementById("ans").value += '9';
+   }
+   function multiply(){
+    document.getElementById("ans").value += '*';
+   }
+   function division(){
+    document.getElementById("ans").value += '/';
+   }
+   function addition(){
+    document.getElementById("ans").value += '+';
+   }
+   function dot(){
+    document.getElementById("ans").value += '.';
+   }
+   function minus(){
+    document.getElementById("ans").value += '-';
+   }
+   function percent(){
+    document.getElementById("ans").value += '%';
+   }
+   function C(){
+    document.getElementById("ans").value = '';
+   }
+   function equal(){
+    document.getElementById("ans").value = eval(document.getElementById("ans").value);
+   }
+  
+  
+   
+   
+
+
